aa4a0548c718a86ba6d1d3b1c826b01677b0d0c0
Himaswin Polaboyena <himaswinpolaboyena@gmail.com>
2026-03-24 19:34:51 -0400

    spring demo

---
+++ spring-first-project/src/test/java/co/pragra/learning/spring_first_project/SpringFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.pragra.learning.spring_first_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
