1e7569b32fb3e253b444ea821bc9cfe747469222
Himaswin Polaboyena <himaswinpolaboyena@gmail.com>
2026-03-30 19:02:15 -0400

    spring

---
+++ NovSpring/src/main/java/io/sample/novspring/NovSpringApplication.java
@@ -0,0 +1,13 @@
+package io.sample.novspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovSpringApplication.class, args);
+    }
+
+}
