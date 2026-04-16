f97dc9179d88615c0b06d7ee0124d72601f5e4f2
Himaswin Polaboyena <himaswinpolaboyena@gmail.com>
2026-04-16 18:00:32 -0400

    connecting to datasource

---
+++ flyway-demo/src/main/resources/db/migration/V2__Seed_Users.sql
@@ -0,0 +1,2 @@
+INSERT INTO TABLE_USER(id, username, password, email) VALUES(1, 'admin', 'temp', 'admin@gmail.com');
+INSERT INTO TABLE_USER(id, username, password, email) VALUES(2, 'user', 'temp', 'user@gmail.com');
---
+++ flyway-demo/src/test/java/io/sample/flywaydemo/FlywayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sample.flywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
